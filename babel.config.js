04e6cb81add9c8bbbea7f6321df9a9c2e6544d8c
José Henrique Leão <jhleao99@gmail.com>
2021-07-16 15:38:38 -0300

    project init

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'inline-dotenv',
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+          alias: {
+            '@screens': './src/screens',
+            '@components': './src/components',
+            '@assets': './src/assets',
+            '@ts': './src/@types',
+            '@globals': './src/globals',
+            '@routes': './src/routes',
+            '@configs': './src/configs',
+            '@services': './src/services',
+            '@utils': './src/utils',
+            '@hooks': './src/hooks',
+          },
+        },
+      ],
+    ],
   };
 };
