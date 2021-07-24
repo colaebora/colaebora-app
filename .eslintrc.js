8238fb5937b419d486425a97b7c2bb3b61db3a36
José Henrique Leão <jhleao99@gmail.com>
2021-07-24 16:01:01 -0300

    add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/style-prop-object': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.tsx'] }],
+    camelcase: 'off',
+  },
+};
