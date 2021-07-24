a193aa5694148d8b0dd8c2c4b37bf4097aef8eea
José Henrique Leão <jhleao99@gmail.com>
2021-07-24 16:02:43 -0300

    add colors and font

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { registerRootComponent } from 'expo';
+import { useFonts } from 'expo-font';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+} from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
     </View>
   );
 };
---
+++ src/globals/style/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    primary: '#69C1CB',
+    primaryBg: '#F5FDFE',
+    primaryText: '#19A4B5',
+
+    text: '#3D3434',
+    lightText: '#7D7D7D',
+
+    gray: '#B7B1B1',
+    lightGray: '#E6E6E6',
+  },
+
+  fonts: {
+    title600: 'Inter_600SemiBold',
+    text500: 'Inter_500Medium',
+    text400: 'Inter_400Regular',
+  },
+};
