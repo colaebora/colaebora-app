814e97ee15699016643d6007c67e99a78431dfb3
José Henrique Leão <jhleao99@gmail.com>
2022-02-01 08:28:02 -0300

    dev: update login page logo

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -3,7 +3,8 @@ import { Image, Text, View } from 'react-native';
 import { AntDesign, Feather } from '@expo/vector-icons';
 import { theme } from '@globals/styles/theme';
 
-import LoginBg from '@assets/login-bg.png';
+import LoginBgTexture from '@assets/login-bg-texture.svg';
+import LoginLogo from '@assets/login-logo.svg';
 import { useAuth } from '@hooks/useAuth';
 import { PrimaryButton } from '@components/Buttons/PrimaryButton';
 import { styles } from './style';
@@ -15,7 +16,13 @@ export const Login: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <Image source={LoginBg} style={styles.image} resizeMode="stretch" />
+      <LoginBgTexture
+        style={styles.loginBgTexture}
+        // width="100%"
+        fill="#fff"
+        fillOpacity={1}
+        opacity={1}
+      />
       <View style={styles.loginSheet}>
         <PrimaryButton
           text="Entrar com o Google"
@@ -26,6 +33,9 @@ export const Login: React.FC = () => {
           <Feather name="log-in" color={theme.colors.primary} size={24} />
           <Text style={styles.skipLoginText}>Entrar como visitante</Text>
         </View>
+      </View>
+      <View style={styles.logoContainer}>
+        <LoginLogo />
       </View>
     </View>
   );
