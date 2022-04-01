c826e4a5b9a511818de2082209d62e52665554e8
BruninTI <bruno.araujo@viannasempre.com.br>
2022-04-01 19:30:45 -0300

    feat: add reactotron

---
+++ ReactotronConfig.ts
@@ -0,0 +1,5 @@
+import Reactotron from 'reactotron-react-native';
+
+Reactotron.configure({ host: '192.168.2.100' }).useReactNative();
+
+export default Reactotron;
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { registerRootComponent } from 'expo';
-import { useFonts } from 'expo-font';
 import {
   Inter_400Regular,
   Inter_500Medium,
   Inter_600SemiBold,
 } from '@expo-google-fonts/inter';
-import AppLoading from 'expo-app-loading';
+import { AddressWizardProvider } from '@hooks/useAddressWIzard';
 import { AuthProvider } from '@hooks/useAuth';
 import { DrawerProvider } from '@hooks/useDrawer';
-import { AddressWizardProvider } from '@hooks/useAddressWIzard';
+import { registerRootComponent } from 'expo';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Reactotron from '../ReactotronConfig';
 import { Routes } from './routes';
+
+if (__DEV__) {
+  Reactotron.connect();
+}
 
 const App = () => {
   const [fontsLoaded] = useFonts({
